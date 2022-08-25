60e5d49b29fddcb058061ef8cf1bc7550d309b4f
codesoliu <soliuahmad99@gmail.com>
2022-08-25 20:56:34 +0100

    first commit

---
+++ src/components/Players.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState, useRef } from 'react'
+import Control from './Control';
+import Details from './Details';
+
+function Players(props) {
+    const audioEl = useRef(null)
+    const [isplaying, setIsPlaying] = useState(false)
+
+    useEffect(() => {
+        if (isplaying) {
+            audioEl.current.play()
+        } else {
+            audioEl.current.pause()
+        }
+    })
+
+    const skipSong = (forwards = true) => {
+        if (forwards) {
+            props.setCurrentSonIndex(() => {
+                let temp = props.currentSongIndex;
+                temp++;
+
+                if (temp > props.songs.length - 1) {
+                    temp = 0
+                }
+
+                return temp;
+            })
+        }
+    }
+    return (
+        <div className='player'>
+            <h4>
+                PLAYING NOW
+            </h4>
+            <Details
+                song={props.songs[props.currentSongIndex]}
+            />
+            <Control
+                isPlaying={isplaying}
+                setIsPlaying={setIsPlaying}
+                skipSong={skipSong}
+            />
+            <audio
+                className='player_audio'
+                src={props.songs[props.currentSongIndex].src}
+                ref={audioEl}
+                controls
+            >
+
+            </audio>
+            <p>
+                Next Up: {" "}
+                <span>
+                    {props.songs[props.nextSongIndex].title} by {" "}
+                    {props.songs[props.nextSongIndex].artist}
+                </span>
+            </p>
+        </div>
+
+    )
+}
+
+export default Players
